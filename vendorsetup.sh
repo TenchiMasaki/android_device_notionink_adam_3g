b633429237eca216e9a34a008a6daeb244774337
Tenchi <tenchimasaki@github.com>
2017-11-09 16:15:08 -0700

    Decatf 8.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,7 +21,7 @@
 # function: add_lunch_combo generic-eng
 export USE_CCACHE=1
 
-add_lunch_combo aosp_adam_3g-userdebug
+add_lunch_combo decatf_adam_3g-userdebug
 
 echo ""
 echo "Patching Workspace..."
